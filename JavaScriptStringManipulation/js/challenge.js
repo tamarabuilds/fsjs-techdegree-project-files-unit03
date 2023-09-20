51b21d9dbdcb85014fa4986626fd94dec59e029f
Tamara Patterson <98510821+tamarabuilds@users.noreply.github.com>
2023-09-19 20:38:22 -0700

    completed string manipulation challenge

--- JavaScriptStringManipulation/js/challenge.js
+++ JavaScriptStringManipulation/js/challenge.js
@@ -4,10 +4,13 @@ function photoName() {
   var name = prompt("Please enter your name");
    
   //remove additional whitespaces
+  name = name.trim()//.replace(' ', '_').toLowerCase()
   
   //replace the space between names with underscore
- 
+  name = name.replace(' ', '_')
+
   //change name to all lowercase
+  name = name.toLowerCase()
     alert("Your photo name is " + name + ".jpg")
     
 }
